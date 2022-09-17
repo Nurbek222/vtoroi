95d7cac84024f536423058f31310ae03b9454817
Nurbekabdyrashitovich <nurbekabdyrashitovich@gmail.com>
2022-09-17 10:56:19 +0300

    29

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-alert('it is true');
+// alert('it is true');
 
-let name_ ="Nurbek";
-console.log(name_);
+// let name_ ="Nurbek";
+// console.log(name_);
 
-let firstname = prompt('Введите имя');
-let lastname;
-let firstname1
-if(firstname =='Nurbek'){
-  lastname = prompt('Введите фамилию');
-}
-else {
+// let firstname = prompt('Введите имя');
+// let lastname;
+// let firstname1
+// if(firstname =='Nurbek'){
+//   lastname = prompt('Введите фамилию');
+// }
+// else {
 	
-		firstname = prompt('введите правильно');
+// 		firstname = prompt('введите правильно');
 	
 
-	if (firstname == 'Nurbek') {
-		lastname = prompt('Введите фамилию');
-	}
-	else {
-		 firstname = prompt('попробуйте еще раз'); 
+// 	if (firstname == 'Nurbek') {
+// 		lastname = prompt('Введите фамилию');
+// 	}
+// 	else {
+// 		 firstname = prompt('попробуйте еще раз'); 
 
-		if (firstname == 'Nurbek') {
-			lastname = prompt('Введите фамилию');
-		}
-		else {
-			firstname = confirm('Прошу обновить страничку');
-		}
-	}
-}
+// 		if (firstname == 'Nurbek') {
+// 			lastname = prompt('Введите фамилию');
+// 		}
+// 		else {
+// 			firstname = confirm('Прошу обновить страничку');
+// 		}
+// 	}
+// }
 
-if(lastname=='Abylov'){
-    console.log('name:'+ firstname,',','lastname:'+ lastname)
-}
+// if(lastname=='Abylov'){
+//     console.log('name:'+ firstname,',','lastname:'+ lastname)
+// }
 
 
 // let number1=+prompt('Ввести число')
@@ -40,3 +40,28 @@ if(lastname=='Abylov'){
 // if(operator1=='+'){
 //   console.log(number1 +number2);
 // }
+
+var arr = ['apple', 'orang', 'pear'];
+arr.forEach(function(item,i,arr) {
+	console.log (i + ':' + item + '(massiv :' + arr+')')
+});
+
+var arrf = [1,9,5,-3,7,8];
+var positivNum = arrf.filter(function(number){
+	return number> 0;
+});
+console.log(positivNum);
+
+var arrf1 = [1,-3,3,6,-6,4];
+function isPositiv(number){
+	return number > 0
+}
+console.log(arrf1.every(isPositiv));
+console.log(arrf1.some(isPositiv));
+
+var names = ['html', 'css', 'javascript'];
+var nameLength = names.map(function(names){
+	return names.length;
+});
+console.log(nameLength)
+
